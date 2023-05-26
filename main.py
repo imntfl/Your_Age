ee114cc579433f4703c7f5e0d9622bc2c439ddb9
NTFL <z1a@ya.ru>
2023-05-26 17:44:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from datetime import date
 dr = input("Введите дату вашего рождения в формате(ГГГГ-MM-ДД): ")
 dr = date.fromisoformat(dr)
 
-vozrast = int((dr.today() - dr).days / 365.25)
+vozrast = int((date.today() - dr).days / 365.25)
 
 print("Вам ", vozrast, " лет")
